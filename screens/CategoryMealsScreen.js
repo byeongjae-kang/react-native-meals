d8aa24089a63ab2c9f04b650f16756b3a9481335
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-10-12 19:03:09 -0700

    receive params from parent component and find the selected category

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
 
 export default function CategoryMealsScreen(props) {
-  console.log(props.navigation);
+  const categoryId = props.route.params.categoryId;
+
+  const selectedCategory = CATEGORIES.find(
+    (category) => category.id === categoryId
+  );
+
   return (
     <View style={styles.container}>
-      <Text>This is CategoryMealsScreen!!</Text>
+      <Text>This is {selectedCategory.name}!!</Text>
       <Button
         title="To Detail"
         onPress={() => props.navigation.navigate("Detail")}
